7448c60867fdeac3d926710aa6b724eb275e81a6
ehakam <ehakam@student.1337.ma>
2020-12-24 18:59:02 +0100

    Twelveth Commit :)

--- engine/ft_init.c
+++ engine/ft_init.c
@@ -1,5 +1,14 @@
 #include "../include/cubengine.h"
 
+void		init_cam(t_cub *cub)
+{
+	cub->cam.mov_dir =		0;
+	cub->cam.rot_dir =		0;
+	cub->cam.side_ang = 	0.0F;
+	cub->cam.mov_spd = 		(TILE_SIZE / 20.0F);
+	cub->cam.rot_spd =		RAD(0.3F);
+}
+
 void		init_cub(t_cub *cub)
 {
 	int		i;
@@ -17,6 +26,27 @@ void		init_cub(t_cub *cub)
 	cub->map = 				NULL;
 	cub->spr = 				NULL;
 	cub->ray = 				NULL;
+	init_cam(cub);
 	while (++i < 6)
 		cub->txt[i].path =	NULL;
 }
+
+void		init_mlx(t_cub *cub)
+{
+	if (!(cub->mlx = mlx_init()))
+		ft_perror( "Error: Failed to initialize mlx!", ft_clean(cub, ERROR));
+	if (!(cub->window = mlx_new_window(cub->mlx, WIN_WIDTH, WIN_HEIGHT, CUBTITLE)))
+		ft_perror( "Error: Failed to initialize Window!", ft_clean(cub, ERROR));
+	if (!(cub->cnvs.img = mlx_new_image(cub->mlx, WIN_WIDTH, WIN_HEIGHT)))
+		ft_perror( "Error: Failed to initialize Canvas!", ft_clean(cub, ERROR));
+	cub->cnvs.data = (int *)mlx_get_data_addr(cub->cnvs.img, &cub->cnvs.bpp,
+											&cub->cnvs.sl, &cub->cnvs.end);
+}
+
+
+
+
+
+
+
+
